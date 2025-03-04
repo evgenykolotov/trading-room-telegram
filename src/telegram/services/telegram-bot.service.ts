f4c11749d06995d7cf8f88ba8264ccb2fea3433d
evgenykolotov <evgenykolotov.dev@gmail.com>
2025-03-04 22:06:16 +0300

    feat(core): Добавлен функционал редактирования через Redis

--- src/telegram/services/telegram-bot.service.ts
+++ src/telegram/services/telegram-bot.service.ts
@@ -1,6 +1,7 @@
 import { Api } from 'telegram';
 import { Telegraf } from 'telegraf';
 import { InjectBot } from 'nestjs-telegraf';
+import { Message, Update } from 'telegraf/types';
 import { Injectable, Logger } from '@nestjs/common';
 import { removeMarkdown } from 'src/common/utils/remove-markdown.utils';
 import { convertEntities } from 'src/common/utils/convert-entities.utils';
@@ -14,18 +15,33 @@ export class TelegramBotService {
   public async sendMessageToChannel(
     channelId: number,
     message: Api.Message,
-  ): Promise<void> {
+  ): Promise<Message.PhotoMessage | Message.TextMessage | undefined> {
     return message.photo
       ? await this.sendPhotoToChannel(channelId, message)
       : await this.sendTextMessageToChannel(channelId, message);
   }
 
+  public async editMessageToChannel(
+    channelId: number,
+    editMessageId: number,
+    message: Api.Message,
+  ): Promise<
+    | true
+    | (Update.Edited & Message.TextMessage)
+    | (Update.Edited & Message.CaptionableMessage)
+    | undefined
+  > {
+    return message.photo
+      ? await this.editPhotoMessageToChannel(channelId, editMessageId, message)
+      : await this.editTextMessageToChannel(channelId, editMessageId, message);
+  }
+
   private async sendTextMessageToChannel(
     channelId: number,
     message: Api.Message,
-  ): Promise<void> {
+  ): Promise<Message.TextMessage | undefined> {
     try {
-      await this.bot.telegram.sendMessage(
+      return await this.bot.telegram.sendMessage(
         channelId,
         removeMarkdown(message.text),
         {
@@ -44,11 +60,11 @@ export class TelegramBotService {
   private async sendPhotoToChannel(
     channelId: number,
     message: Api.Message,
-  ): Promise<void> {
+  ): Promise<Message.PhotoMessage | undefined> {
     try {
       const media = await message.downloadMedia();
       if (media instanceof Buffer) {
-        await this.bot.telegram.sendPhoto(
+        return await this.bot.telegram.sendPhoto(
           channelId,
           { source: media },
           {
@@ -60,6 +76,53 @@ export class TelegramBotService {
     } catch (error: unknown) {
       this.logger.error(
         `Ошибка при отправке сообщения c фото в канал ${channelId}`,
+        error,
+      );
+    }
+  }
+
+  private async editTextMessageToChannel(
+    channelId: number,
+    editMessageId: number,
+    message: Api.Message,
+  ): Promise<true | (Update.Edited & Message.TextMessage) | undefined> {
+    try {
+      return await this.bot.telegram.editMessageText(
+        channelId,
+        editMessageId,
+        undefined,
+        removeMarkdown(message.text),
+        {
+          link_preview_options: { is_disabled: true },
+          entities: convertEntities(message.entities ?? []),
+        },
+      );
+    } catch (error: unknown) {
+      this.logger.error(
+        `Ошибка при редактировании текстового сообщения в канал ${channelId}`,
+        error,
+      );
+    }
+  }
+
+  private async editPhotoMessageToChannel(
+    channelId: number,
+    editMessageId: number,
+    message: Api.Message,
+  ): Promise<true | (Update.Edited & Message.CaptionableMessage) | undefined> {
+    try {
+      return await this.bot.telegram.editMessageCaption(
+        channelId,
+        editMessageId,
+        undefined,
+        removeMarkdown(message.text),
+        {
+          caption_entities: convertEntities(message.entities ?? []),
+        },
+      );
+    } catch (error: unknown) {
+      this.logger.error(
+        `Ошибка при редактировании сообщения c фото в канал ${channelId}`,
         error,
       );
     }
--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegramService } from './services/telegram.service';
 import { TelegramBotService } from './services/telegram-bot.service';
 import { TelegramClientProvider } from './providers/telegram-client.provider';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
   imports: [
@@ -12,6 +13,17 @@ import { TelegramClientProvider } from './providers/telegram-client.provider';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         token: configService.get<string>('TELEGRAM_BOT_TOKEN') ?? '',
+      }),
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: configService.get<string>('REDIS_URL') ?? '',
+        options: {
+          password: configService.get<string>('REDIS_PASSWORD') ?? '',
+        },
       }),
     }),
   ],
